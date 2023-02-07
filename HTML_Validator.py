8310cddf84957d608847652c8b19c8c77933cf12
Luis Gomez <lgomez25@cmc.edu>
2023-02-07 10:55:40 -0800

    HW 3

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,9 +1,12 @@
 #!/bin/python3
-import re 
+import re
+
 
 def validate_html(html):
+
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    This function performs a limited version of html validation
+    by checking whether every opening tag has a corresponding closing tag.
 
     >>> validate_html('<strong>example</strong>')
     True
@@ -15,7 +18,7 @@ def validate_html(html):
     if len(tags) <= 1 and len(html) > 0:
         return False
     for i in range(len(tags)):
-        if  '/' not in tags[i]:
+        if '/' not in tags[i]:
             stack.append(tags[i])
         else:
             if len(stack) == 0:
@@ -29,21 +32,28 @@ def validate_html(html):
     else:
         return False
     # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the class/book
-    # the main difference between your code and the code from class will be that you will have to keep track of not just the 3 types of parentheses,
+    # use the _extract_tags function below to generate a list of html
+    # tags without any extra text;
+    # then process these html tags using the balanced parentheses
+    # algorithm from the class/book
+    # the main difference between your code and the code
+    # from class will be that you will have to keep
+    # track of not just the 3 types of parentheses,
     # but arbitrary text located between the html tags
 
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`. By convention in Python, helper functions 
-    that are not meant to be used directly by the user are preixed with an underscore.
-    This function returns a list of all the html tags contained in the input string,
+    This is a helper function for `validate_html`. By convention
+    in Python, helper functions
+    that are not meant to be used directly by the user
+    are preixed with an underscore.
+    This function returns a list of all the
+    html tags contained in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
-    extracted_tags= re.findall('<[\/a-z0-9]+>', html)
+    extracted_tags = re.findall('<[\/a-z0-9]+>', html)
     return extracted_tags
