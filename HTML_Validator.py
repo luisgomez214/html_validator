875e6c4a110905ba2c4f10c1bb9c5461c8273e35
Luis Gomez <lgomez25@cmc.edu>
2023-02-07 10:40:55 -0800

    Hw 3

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-
+import re 
 
 def validate_html(html):
     '''
@@ -10,7 +10,24 @@ def validate_html(html):
     >>> validate_html('<strong>example')
     False
     '''
-
+    tags = _extract_tags(html)
+    stack = []
+    if len(tags) <= 1 and len(html) > 0:
+        return False
+    for i in range(len(tags)):
+        if  '/' not in tags[i]:
+            stack.append(tags[i])
+        else:
+            if len(stack) == 0:
+                return False
+            if stack[-1][1:] in tags[i]:
+                stack.pop()
+            else:
+                return False
+    if len(stack) == 0:
+        return True
+    else:
+        return False
     # HINT:
     # use the _extract_tags function below to generate a list of html tags without any extra text;
     # then process these html tags using the balanced parentheses algorithm from the class/book
@@ -20,12 +37,13 @@ def validate_html(html):
 
 def _extract_tags(html):
     '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
-
+    This is a helper function for `validate_html`. By convention in Python, helper functions 
+    that are not meant to be used directly by the user are preixed with an underscore.
     This function returns a list of all the html tags contained in the input string,
     stripping out all text not contained within angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
+    extracted_tags= re.findall('<[\/a-z0-9]+>', html)
+    return extracted_tags
